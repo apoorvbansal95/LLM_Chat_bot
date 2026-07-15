fad1168cb1205ef1e3abc11cedf2d2309b61b56b
Apoorv Bansal <apoorvbansal@192.168.1.5>
2026-07-15 12:19:23 +0530

    Sqlite3

---
+++ agentic_chatbot_db_backend.py
@@ -0,0 +1,54 @@
+from langgraph.graph import StateGraph , START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+import sqlite3
+load_dotenv()   
+llm = ChatOpenAI()
+
+
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state:ChatState):
+    messages=state['messages']
+    
+    #send to llm 
+    response = llm.invoke(messages)
+    #response store state
+    return {'messages': [response]}
+
+conn=sqlite3.connect(database="chatbot.db", check_same_thread=False)
+checkpoint=  SqliteSaver(conn)
+graph= StateGraph(ChatState)
+
+#add nodes
+graph.add_node('chat_node', chat_node)
+
+#add edges
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot=graph.compile(checkpointer = checkpoint)
+
+def get_All_threads():
+    all_threads=set()
+    threads=checkpoint.list(None)
+    for thread in threads:
+        all_threads.add(thread.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
+
+# CONFIG={'configurable':{"thread_id":"default_thread"}}
+
+# res=chatbot.invoke(
+#     {"message":[HumanMessage(content="Hello, how are you")]},
+#     config=CONFIG
+# )
+# print(res)
--- app.py
+++ app.py
@@ -1,7 +1,43 @@
 from agentic_chatbot_backend import chatbot
 from langchain_core.messages import BaseMessage, HumanMessage
-thread_id="1"
-config={"configurable":{"thread_id":thread_id}}
-response = chatbot.invoke({'messages':[HumanMessage(content="What is the capital of India")]}, config=config)
+import streamlit as st
 
-print(response['messages'][-1].content)
+# thread_id="1"
+# config={"configurable":{"thread_id":thread_id}}
+# response = chatbot.invoke({'messages':[HumanMessage(content="What is the capital of India")]}, config=config)
+
+# print(response['messages'][-1].content)
+
+
+st.title("Agentic chatbot with Langgraph")
+CONFIG={'configurable':{'thread_id': 'thread-1'}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']=[]
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+st.title("Agentic chatbot with Langgraph")
+user_input= st.chat_input('Type here')
+st.write('User: ', user_input )
+if user_input:
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+
+     # first add the message to message_history
+    with st.chat_message('assistant'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= CONFIG,
+                stream_mode= 'messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
